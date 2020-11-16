e758d7b347f66dd4f1da6d61e67530aae49cc679
Ariel Latour <ariel-l.ariel.latour@hotmail.com>
2020-11-16 14:16:17 -0300

    first fix

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,20 +1,21 @@
 import random
 
 def difficulty(mode):
-    lifes_numbers = {}
-    test = []
+    lifes_and_numbers = {}
+    li_nu = []
+    mode = mode.lower()
     if mode == "easy" or mode == "medium" or mode == "hard":
-        lifes_numbers["lifes"] = 7
-        lifes_numbers["number_to_guess"] = random.randint(0,10)
+        lifes_and_numbers["lifes"] = 7
+        lifes_and_numbers["number_to_guess"] = random.randint(0,10)
         if mode == "medium":
-            lifes_numbers["lifes"] = 5
-            lifes_numbers["number_to_guess"] = random.randint(0,100)
+            lifes_and_numbers["lifes"] = 5
+            lifes_and_numbers["number_to_guess"] = random.randint(0,100)
         elif mode == "hard":
-            lifes_numbers["lifes"] = 3
-            lifes_numbers["number_to_guess"] = random.randint(0,1000)
-    for v in lifes_numbers.values():
-        test.append(v)
-    return test
+            lifes_and_numbers["lifes"] = 3
+            lifes_and_numbers["number_to_guess"] = random.randint(0,1000)
+    for v in lifes_and_numbers.values():
+        li_nu.append(v)
+    return li_nu
 
 def hint(actual_number, your_number):
     if your_number > actual_number:
@@ -24,38 +25,36 @@ def hint(actual_number, your_number):
 
 def game(mode):
     lifes, number_to_guess = difficulty(mode)[0], difficulty(mode)[1]
-    print(lifes, number_to_guess)
-    text_0 = "-- Guess the number --"
-    text_0 += "\nPlease, enter your number: "
-    want_continue = "Do you want to play again? 'y/n' "
-    play_again = "Wich difficulty do you want to play? (easy, medium, hard): "
+    introduction = "-- Guess the number --"
+    introduction += "\nPlease, enter your number: "
+    play_again = "Do you want to play again? 'y/n' "
+    choose_difficulty = "Wich difficulty do you want to play? (easy, medium, hard): "
     while lifes != 0:
-        your_number = int(input(text_0))
+        your_number = int(input(introduction))
         if number_to_guess != your_number:
             print(f"WRONG!!{hint(number_to_guess, your_number)}")
             lifes -= 1
             if lifes == 0:
                 print("GAME OVER")
-                want_continue = (input(want_continue))
-                if want_continue == "n":
+                play_again = (input(play_again))
+                if play_again == "n":
                     lifes = 0
                 else:
-                    play_again = input(play_again)
-                    difficulty(mode)
+                    input(choose_difficulty)
                     lifes, number_to_guess = difficulty(mode)[0], difficulty(mode)[1]
+                    print(lifes, number_to_guess)
         else:
-            print("Nice, you guessed the number")
-            want_continue = (input(want_continue))
-            if want_continue == "n":
+            print("Congrats!! You guessed the number")
+            play_again = (input(play_again))
+            if play_again == "n":
                 lifes = 0
             else:
-                play_again = input(play_again)
-                difficulty(mode)
+                input(choose_difficulty)
                 lifes, number_to_guess = difficulty(mode)[0], difficulty(mode)[1]
     return "Thanks for playing!!"
 
 welcome = "Welcome to the game. "
 welcome += "\nWich difficulty do you want to play? (easy, medium, hard): "
-choose_difficulty = input(welcome)
+select_difficulty = input(welcome)
 
-print(game(choose_difficulty))
+print(game(select_difficulty))
